7a4d9e446e44847a29842a9d06a2b6e5bce90fde
Tim <tsclay9@gmail.com>
2020-06-26 16:03:41 -0400

    skeleton of user controller

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const User = require('../models/User')
+
+const user = express.Router()
+
+// Get user data
+user.get('/', (req, res) => {
+  //
+})
+
+// Create new user
+user.post('/', (req, res) => {
+  //
+})
+
+// Update user details
+user.put('/', (req, res) => {
+  //
+})
+
+// Delete user account
+user.delete('/', (req, res) => {
+  //
+})
